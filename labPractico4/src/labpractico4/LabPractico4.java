0358315ab9ee9f9ec117d9d4563365c319e83e73
Santytft <Santiagoacoria@sanluis.edu.ar>
2023-08-31 16:38:06 -0300

    Commit

---
+++ labPractico4/src/labpractico4/LabPractico4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package labpractico4;
+
+/**
+ *
+ * @author SantyTFT
+ */
+public class LabPractico4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
